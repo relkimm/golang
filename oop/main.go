ed90232647a6dec130775a89ebe5172d47232f69
relkimm <bbonggu92@gmail.com>
2023-01-16 23:16:28 +0900

    oop: define dog & cat class

---
+++ oop/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type Dog struct {
+	name string
+}
+
+func NewDog(name string) Dog {
+	return Dog{name: name}
+}
+
+func (d Dog) Sounds() {
+	fmt.Printf("%s : wolf! wolf!\n", d.name)
+}
+
+type Cat struct { 
+	name string
+}
+
+func NewCat(name string) Cat {
+	return Cat{name: name}
+}
+
+func (c Cat) Sounds() {
+	fmt.Printf("%s : meow.\n", c.name)
+}
+
+func main() {
+	dog := NewDog("Charlie")
+	dog2 := dog
+	dog2.name = "Happy"
+	
+	dog.Sounds()
+	dog2.Sounds()
+	
+	cat := NewCat("Barley")
+	cat2 := cat
+	cat2.name = "Sad"
+	
+	cat.Sounds()
+	cat2.Sounds()
+}
